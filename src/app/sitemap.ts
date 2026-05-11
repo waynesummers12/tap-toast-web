ded2851f84bc7c07dfb9d3582d20d334007269df
Wayne Summers <waynesummers12@gmail.com>
2026-05-10 20:06:54 -0600

    new pages

---
+++ src/app/sitemap.ts
@@ -0,0 +1,44 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://coloradotapandtoast.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/dirty-soda-bar",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/dirty-soda-bar-denver",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/dirty-soda-bar-littleton",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/dirty-soda-bar-lakewood",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/dirty-soda-bar-parker",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/dirty-soda-bar-highlands-ranch",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/dirty-soda-bar-centennial",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/what-is-dirty-soda-bar",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://coloradotapandtoast.com/book",
+      lastModified: new Date(),
+    },
+  ]
+}
